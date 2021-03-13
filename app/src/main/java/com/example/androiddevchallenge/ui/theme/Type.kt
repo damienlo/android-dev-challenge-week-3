676f9423379b7b8898e3d2dfc1eb49fd07c54e2b
Damien Locque <damien.locque@yoti.com>
2021-03-13 09:19:56 +0000

    initial theme + font + colors

--- app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/theme/Type.kt
@@ -17,27 +17,75 @@ package com.example.androiddevchallenge.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.R
 
-// Set of Material typography styles to start with
-val typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+val kulimParkFontFamily = FontFamily(
+    Font(
+        R.font.kulim_park_light,
+        weight = FontWeight.Light,
+        style = FontStyle.Normal
+    ),
+    Font(
+        R.font.kulim_park_regular,
+        weight = FontWeight.Normal,
+        style = FontStyle.Normal
     )
-        /* Other default text styles to override
+)
+val latoFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            R.font.lato_regular,
+            weight = FontWeight.Normal,
+            style = FontStyle.Normal
+        ),
+        Font(
+            R.font.lato_bold,
+            weight = FontWeight.Bold,
+            style = FontStyle.Normal
+        )
+    )
+)
+
+val typography = Typography(
+    h1 = TextStyle(
+        fontFamily = kulimParkFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 28.sp,
+        letterSpacing = 1.15.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = kulimParkFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        letterSpacing = 0.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = latoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = latoFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        letterSpacing = 0.sp
+    ),
     button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontFamily = latoFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 1.15.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = kulimParkFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = 12.sp,
+        letterSpacing = 1.15.sp
     )
-    */
 )
