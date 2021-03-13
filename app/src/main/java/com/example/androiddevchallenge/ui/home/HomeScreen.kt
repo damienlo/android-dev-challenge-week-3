12f3e6665768aaf5b36c95dfc5e47043cf877b2a
Damien Locque <damien.locque@yoti.com>
2021-03-13 13:01:03 +0000

    apply spotless

--- app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/home/HomeScreen.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.home
 
 import androidx.compose.foundation.Image
@@ -17,11 +32,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import dev.chrisbanes.accompanist.insets.navigationBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomNavigation
@@ -30,7 +40,9 @@ import androidx.compose.material.Card
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
@@ -43,11 +55,14 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.R
 import com.example.androiddevchallenge.data.DummyItem
 import com.example.androiddevchallenge.data.HomeDummyItems
+import dev.chrisbanes.accompanist.insets.navigationBarsPadding
 
 @Composable
 fun HomeScreen() {
@@ -118,7 +133,8 @@ fun HomeScreen() {
                 LazyRow(contentPadding = PaddingValues(start = 8.dp)) {
                     items(
                         HomeDummyItems.favoriteList.chunked(2)
-                            .map { it[0] to it[1] }) { itemContent ->
+                            .map { it[0] to it[1] }
+                    ) { itemContent ->
                         MySoothItemCardListVerticalItem(itemContent)
                     }
                 }
@@ -165,7 +181,6 @@ fun HomeScreen() {
                     }
                 }
             }
-
         }
     }
 }
@@ -199,7 +214,7 @@ fun MySoothItemCardListItem(itemContent: DummyItem) {
             Image(
                 painter = painterResource(id = itemContent.image),
                 contentDescription = itemContent.text,
-                contentScale = ContentScale.FillBounds,
+                contentScale = ContentScale.Crop,
                 modifier = Modifier.aspectRatio(1f)
             )
             Spacer(modifier = Modifier.height(8.dp))
@@ -224,7 +239,7 @@ fun MySoothImageListItem(itemContent: DummyItem) {
         Image(
             painter = painterResource(id = itemContent.image),
             contentDescription = itemContent.text,
-            contentScale = ContentScale.FillBounds,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .width(88.dp)
                 .height(88.dp)
